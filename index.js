f6da25514cc1d9d1e39e624b323e8cb9413d46e9
phanhoang10 <phanhoang1022002@gmail.com>
2023-12-22 16:09:01 +0700

    second

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, Node.js update project first!');
+  res.end('Hello, Node.js update project second!');
 });
 
 const PORT = 3000;
